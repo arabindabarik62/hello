dd9dcef5b56269faf20ad07703024083d89dad11
arabindabarik62 <arabindabarik62@outlook.com>
2019-02-11 17:22:28 +0530

    this first

--- demotest.py
+++ demotest.py
@@ -3,6 +3,8 @@ class test:
         print("hello this is arabinda")
     def demo(self):
         print("this is arabinda barik")
+    def demo1(self):
+        print("this is hadoop jobs")
 
 dd = test()
 dd.demo()
